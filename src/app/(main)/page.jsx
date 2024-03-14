d1167efbf14f09cbd6e1aef50984a0784f64082a
agilistikmal <agilistikmal3@gmail.com>
2024-03-14 09:14:11 +0700

    Product Overview Page

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <div className="w-full max-w-screen-xl mx-auto px-8 text-cyan-900 space-y-64 pb-96">
         <div id="hero" className="text-center mt-16 space-y-8">
-          <h1 className="font-bold text-4xl md:text-[128px] leading-none">
+          <h1 className="font-bold font-baloo text-4xl md:text-[128px] leading-none">
             Consistency is A Key
           </h1>
           <h2>
@@ -57,8 +57,14 @@ export default function Home() {
           </div>
         </div>
         <div id="add_merch">
-          <h1 className="text-center font-bold text-3xl sm:text-4xl lg:text-6xl p-2">Dapatkan Merchandise Dari Lorem Ipsum Der Silit Ast Lorem Ipsum</h1>
-          <p className="text-center p-6 text-lg">Lorem ipsum dolor sit amet consectetur. Tincidunt quam risus neque quam laoreet. Ac quis neque ut cras velit. Porta et malesuada neque rhoncus porttitor. Sapien dictum vitae lectus diam eget.</p>
+          <h1 className="text-center font-bold text-3xl sm:text-4xl lg:text-6xl p-2">
+            Dapatkan Merchandise Dari Lorem Ipsum Der Silit Ast Lorem Ipsum
+          </h1>
+          <p className="text-center p-6 text-lg">
+            Lorem ipsum dolor sit amet consectetur. Tincidunt quam risus neque
+            quam laoreet. Ac quis neque ut cras velit. Porta et malesuada neque
+            rhoncus porttitor. Sapien dictum vitae lectus diam eget.
+          </p>
           <div className="flex flex-row justify-center">
             <Button type={2}>Merchandise</Button>
           </div>
--- src/components/review.jsx
+++ src/components/review.jsx
@@ -5,27 +5,27 @@ export default function Review({ profilePicture, nama, rating, ulasan, date }) {
   return (
     <>
       <div className="p-8">
-        <div className="grid grid-cols-[6%,94%] gap-4">
-          <div className="  place-items-stretch">
+        <div className="flex gap-4">
+          <div className="place-items-stretch">
             <Image
               src={profilePicture}
-              width={84}
-              height={84}
+              width={64}
+              height={64}
               alt="profile picture"
               className="rounded-full"
             />
           </div>
-          <div className="flex-grow ">
-            <h1 className="font-bold text-3xl text-cyan-900">{nama}</h1>
-            <div className=" mx-auto mt-4 inline-flex gap-4">
+          <div className="flex-grow">
+            <h1 className="font-semibold text-xl text-cyan-900">{nama}</h1>
+            <div className="flex">
               {[...Array(rating)].map((_, i) => (
                 <div key={i}>
-                  <StarIcon />
+                  <StarIcon className="w-6" />
                 </div>
               ))}
             </div>
-            <p className="mt-4">{ulasan}</p>
-            <p className="mt-4">{date}</p>
+            <p>{ulasan}</p>
+            <p className="text-sm text-cyan-900/70">{date}</p>
           </div>
         </div>
       </div>
