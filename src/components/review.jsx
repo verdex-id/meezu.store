7b8bb340248042f2d6722ab3667181f5ede73361
agilistikmal <agilistikmal3@gmail.com>
2024-03-14 08:37:00 +0700

    Review Components

---
+++ src/components/review.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import StarIcon from "@/icons/star";
+
+export default function Review({ profilePicture, nama, rating, ulasan, date }) {
+  return (
+    <>
+      <div className="p-8">
+        <div className="grid grid-cols-[6%,94%] gap-4">
+          <div className="  place-items-stretch">
+            <Image
+              src={profilePicture}
+              width={84}
+              height={84}
+              alt="profile picture"
+              className="rounded-full"
+            />
+          </div>
+          <div className="flex-grow ">
+            <h1 className="font-bold text-3xl text-cyan-900">{nama}</h1>
+            <div className=" mx-auto mt-4 inline-flex gap-4">
+              {[...Array(rating)].map(() => (
+                <StarIcon />
+              ))}
+            </div>
+            <p className="mt-4">{ulasan}</p>
+            <p className="mt-4">{date}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
