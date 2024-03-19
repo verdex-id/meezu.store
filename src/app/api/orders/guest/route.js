efd0fc86168f268e18c44806ca3a4db6b4e0a6c2
rizkia-as-pac <rizkia.as.pac@gmail.com>
2024-03-19 13:53:34 +0000

    -

--- src/app/api/orders/guest/route.js
+++ src/app/api/orders/guest/route.js
@@ -1,25 +1,36 @@
-import prisma from "@/lib/prisma";
 import Joi from "joi";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const schema = Joi.object({
-        guest_full_name: Joi.string(),
-        guest_phone_number: Joi.string(),
-        guest_email: Joi.string(),
-        guest_address: Joi.string(),
-        discount_code: Joi.string(),
-        payment_method: Joi.string(),
-        courier_service_code: Joi.string(),
-        order_items: Joi.array(Joi.object({
-            product_iteration_id: Joi.string(),
-            order_item_quantity: Joi.string(),
-        }))
+
+    const req = await request.json()
+
+     
+
+    const createdOrder = await prisma.order.create({
+        data: {
+            order_status: awaitingPayment,
+
+
+        }
     })
 
-
-    const createdOrder = await prisma.order.create({})
+    return NextResponse.json({ req: req })
 
 
 
 
 }
+const schema = Joi.object({
+    guest_full_name: Joi.string(),
+    guest_phone_number: Joi.string(),
+    guest_email: Joi.string(),
+    guest_address: Joi.string(),
+    discount_code: Joi.string(),
+    payment_method: Joi.string(),
+    courier_service_code: Joi.string(),
+    order_items: Joi.array().items(Joi.object({
+        product_iteration_id: Joi.string(),
+        order_item_quantity: Joi.string(),
+    }))
+})
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,1 +1,8 @@
+import { NextResponse } from "next/server"
+
+const { func } = require("joi")
+
 // API ROUTE HERE
+export async function GET() {
+    return NextResponse.json({ tes: "tes" })
+}
