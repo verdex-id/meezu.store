f891e935af0457c7034f1ee0a5725837adfcd4e1
Rizkia Adhy Syahputra <rizkia.as.actmp@gmail.com>
2024-04-18 11:58:39 +0700

    feat(utils): add function to generate order code

--- src/utils/random.js
+++ src/utils/random.js
@@ -8,3 +8,16 @@ export function generateRandomString(length) {
   }
   return randomString;
 }
+
+export function generateOrderCode() {
+  const date = new Date();
+  const year = date.getFullYear().toString().substring(2);
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const milliseconds = date.getMilliseconds().toString().padStart(3, "0");
+  const randomStr = Math.random().toString(36).substring(2, 8);
+  return `ORD-${year}${month}${day}-${hours}${minutes}${seconds}${milliseconds}-${randomStr.toUpperCase()}`;
+}
