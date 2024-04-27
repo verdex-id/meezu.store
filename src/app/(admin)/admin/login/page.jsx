8a48fafa1f2d7c82649e709277ad9b1401cc642f
agilistikmal <agilistikmal3@gmail.com>
2024-04-27 14:00:00 +0700

    Products API Impl

--- src/app/(admin)/admin/login/page.jsx
+++ src/app/(admin)/admin/login/page.jsx
@@ -1,48 +1,87 @@
+"use client";
+
 import Button from "@/components/button";
 import Input from "@/components/input";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function AdminLoginPage() {
-    return (
-        <>
-            <div className="min-h-dvh w-full max-w-screen-xl mx-auto text-cyan-900">
-                <div>
-                    <h1 className="text-center font-baloo font-bold text-3xl sm:text-4xl lg:text-6xl p-12 mt-12">Admin Dashboard</h1>
-                </div>
-                <div className="grid grid-cols-2 gap-8 p-4 mt-8">
-                    <div>
-                        <div className="mt-8">
-                            <Input 
-                                type={"text"}
-                                id={"username"}
-                                name={"username"}
-                                title={"Username"}
-                                placeholder="Username"
-                            />
-                        </div>
-                        <div className="mt-8">
-                            <Input 
-                                type={"text"}
-                                id={"password"}
-                                name={"password"}
-                                title={"Password"}
-                                placeholder="Password"
-                            />
-                        </div>
-                        <div className="mt-12">
-                            <Button type={2}>Masuk</Button>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-end">
-                        <Image
-                            src={"/logo/akudav2.png"}
-                            alt="logo"
-                            width={505}
-                            height={505}
-                        />
-                    </div>
-                </div>
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [errors, setErrors] = useState();
+  const [loading, setLaoding] = useState(false);
+
+  async function handleLogin() {
+    setLaoding(true);
+    const res = await fetch("/api/admins/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((r) => r.json());
+
+    if (res.status == "fail") {
+      setErrors(res.message);
+    } else {
+      window.location.replace("/admin/dashboard");
+    }
+    setLaoding(false);
+  }
+  return (
+    <>
+      <div className="min-h-dvh w-full max-w-screen-xl mx-auto text-cyan-900">
+        <div>
+          <h1 className="text-center font-baloo font-bold text-3xl sm:text-4xl lg:text-6xl p-12 mt-12">
+            Admin Dashboard
+          </h1>
+        </div>
+        <div className="grid grid-cols-2 gap-8 p-4 mt-8">
+          <div>
+            {errors && (
+              <div className="mt-8 bg-red-300 border-l-4 border-red-500 p-5">
+                Error: {errors}
+              </div>
+            )}
+            <div className="mt-8">
+              <Input
+                type={"text"}
+                id={"email"}
+                name={"email"}
+                title={"Email"}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-        </>
-    )
+            <div className="mt-8">
+              <Input
+                type={"password"}
+                id={"password"}
+                name={"password"}
+                title={"Password"}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={`mt-12 ${loading && "animate-pulse"}`}>
+              <Button onClick={handleLogin} type={2}>
+                {loading ? "Loading..." : "Masuk"}
+              </Button>
+            </div>
+          </div>
+          <div className="flex items-center justify-end">
+            <Image
+              src={"/logo/akudav2.png"}
+              alt="logo"
+              width={505}
+              height={505}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/api/admins/login/route.js
+++ src/app/api/admins/login/route.js
@@ -4,87 +4,93 @@ import { errorResponse, failResponse, successResponse } from "@/utils/response";
 import Joi from "joi";
 import { comparePassword } from "@/lib/password";
 import prisma from "@/lib/prisma";
+import { cookies } from "next/headers";
 
 export async function POST(request) {
-    const schema = Joi.object({
-        password: Joi.string().required(),
-        email: Joi.string().email().required(),
-    });
+  const schema = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().email().required(),
+  });
 
-    const req = await request.json();
+  const req = await request.json();
 
-    const invalidReq = schema.validate(req);
-    if (invalidReq.error) {
-        return NextResponse.json(
-            ...failResponse("Invalid request format.", 400, invalidReq.error.details),
-        );
-    }
-
-    const admin = await prisma.admin.findUnique({
-        where: {
-            admin_email: req.email,
-        },
-    });
-
-    if (!admin) {
-        return NextResponse.json(
-            ...failResponse("Username and/or password are incorrect.", 401),
-        );
-    }
-
-    const isCorrectPassword = await comparePassword(
-        req.password,
-        admin.admin_hashedPassword,
+  const invalidReq = schema.validate(req);
+  if (invalidReq.error) {
+    return NextResponse.json(
+      ...failResponse("Invalid request format.", 400, invalidReq.error.details)
     );
+  }
 
-    if (!isCorrectPassword) {
-        return NextResponse.json(
-            ...failResponse("Username and/or password are incorrect.", 401),
-        );
-    }
+  const admin = await prisma.admin.findUnique({
+    where: {
+      admin_email: req.email,
+    },
+  });
 
-    const accessToken = await createToken(
-        admin.admin_id,
-        process.env.ACCESS_TOKEN_DURATION,
+  if (!admin) {
+    return NextResponse.json(
+      ...failResponse("Username and/or password are incorrect.", 401)
     );
+  }
 
-    if (accessToken.error) {
-        return NextResponse.json(...errorResponse());
-    }
+  const isCorrectPassword = await comparePassword(
+    req.password,
+    admin.admin_hashedPassword
+  );
 
-    const refreshToken = await createToken(
-        admin.admin_id,
-        process.env.REFRESH_TOKEN_DURATION,
+  if (!isCorrectPassword) {
+    return NextResponse.json(
+      ...failResponse("Username and/or password are incorrect.", 401)
     );
+  }
 
-    if (refreshToken.error) {
-        return NextResponse.json(...errorResponse());
-    }
+  const accessToken = await createToken(
+    admin.admin_id,
+    process.env.ACCESS_TOKEN_DURATION
+  );
 
-    const createdSession = await prisma.session.create({
-        data: {
-            session_id: refreshToken.payload.id,
-            admin_id: admin.admin_id,
-            refresh_token: refreshToken.token,
-            expired_at: refreshToken.payload.expiredAt,
-        },
-    });
+  if (accessToken.error) {
+    return NextResponse.json(...errorResponse());
+  }
 
-    if (!createdSession) {
-        return NextResponse.json(...errorResponse());
-    }
+  const refreshToken = await createToken(
+    admin.admin_id,
+    process.env.REFRESH_TOKEN_DURATION
+  );
 
-    const res = {
-        session_id: createdSession.admin_id,
-        access_token: accessToken.token,
-        access_token_expire_at: accessToken.payload.expiredAt,
-        refresh_token: refreshToken.token,
-        refresh_token_expire_at: refreshToken.payload.expiredAt,
-        admin: {
-            full_name: admin.fullName,
-            email: admin.email,
-        },
-    };
+  if (refreshToken.error) {
+    return NextResponse.json(...errorResponse());
+  }
 
-    return NextResponse.json(...successResponse(res));
+  const createdSession = await prisma.session.create({
+    data: {
+      session_id: refreshToken.payload.id,
+      admin_id: admin.admin_id,
+      refresh_token: refreshToken.token,
+      expired_at: refreshToken.payload.expiredAt,
+    },
+  });
+
+  if (!createdSession) {
+    return NextResponse.json(...errorResponse());
+  }
+
+  const res = {
+    session_id: createdSession.admin_id,
+    access_token: accessToken.token,
+    access_token_expire_at: accessToken.payload.expiredAt,
+    refresh_token: refreshToken.token,
+    refresh_token_expire_at: refreshToken.payload.expiredAt,
+    admin: {
+      full_name: admin.fullName,
+      email: admin.email,
+    },
+  };
+
+  const cookie = cookies();
+  cookie.set("session_id", res.session_id);
+  cookie.set("access_token", res.access_token);
+  cookie.set("refresh_token", res.refresh_token);
+
+  return NextResponse.json(...successResponse(res));
 }
