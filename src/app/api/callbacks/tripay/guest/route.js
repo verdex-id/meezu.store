9d9ab890ef63f2471984fc4dfb932837cca71ecc
Rizkia Adhy Syahputra <rizkia.as.actmp@gmail.com>
2024-04-30 22:40:56 +0700

    fix(api/callback/tripay): refactor code and reduce db transaction workloads

--- src/app/api/callbacks/tripay/guest/route.js
+++ src/app/api/callbacks/tripay/guest/route.js
@@ -9,38 +9,34 @@ import { makeFailedStatus } from "./make-failed-status";
 import { makeRefundStatus } from "./make-refund-status";
 
 export async function POST(request) {
-  const callbackSignature = headers().get("X-Callback-Signature");
-  const callbackEvent = headers().get("X-Callback-Event");
-  let req = await request.json();
+  try {
+    const callbackSignature = headers().get("X-Callback-Signature");
+    const callbackEvent = headers().get("X-Callback-Event");
+    let req = await request.json();
 
-  const jsonString = JSON.stringify(req);
-  req = JSON.parse(jsonString);
+    const jsonString = JSON.stringify(req);
+    req = JSON.parse(jsonString);
 
-  if (req === null) {
-    return NextResponse.json(
-      ...tripayResponse(false, "Invalid data sent by tripay", 400),
-    );
-  }
+    if (req === null) {
+      throw new FailError("Invalid data sent by tripay", 400);
+    }
 
-  const signature = tripayCallbackSignature(jsonString);
+    const signature = tripayCallbackSignature(jsonString);
 
-  if (signature !== callbackSignature) {
-    return NextResponse.json(
-      ...tripayResponse(false, "Invalid signature", 400),
-    );
-  }
+    if (signature !== callbackSignature) {
+      throw new FailError("Invalid signature", 400);
+    }
 
-  if (callbackEvent !== "payment_status") {
-    return NextResponse.json(
-      ...tripayResponse(
-        false,
+    if (callbackEvent !== "payment_status") {
+      throw new FailError(
         "Unrecognized callback event, no action was taken",
         400,
-      ),
-    );
-  }
+      );
+    }
 
-  if (req.is_closed_payment === 1) {
+    if (!req.is_closed_payment === 1) {
+      throw new FailError("Open payment not supported", 400);
+    }
     const order = await prisma.order.findUnique({
       where: {
         order_code: req.merchant_ref,
@@ -48,67 +44,67 @@ export async function POST(request) {
           paygate_transaction_id: req.reference,
           payment_method: req.payment_method_code,
         },
-        invoice: {},
       },
       select: {
         order_id: true,
         order_status: true,
-        invoice: true,
+        discount_code: true,
+        invoice: {
+          select: {
+            invoice_item: true,
+            payment_status: true,
+          },
+        },
       },
     });
 
     if (!order) {
-      return NextResponse.json(
-        ...tripayResponse(
-          false,
-          `No order found with merchant reference : ${req.merchant_ref}`,
-          404,
-        ),
+      throw new FailError(
+        `No order found with merchant reference : ${req.merchant_ref}`,
+        404,
       );
     }
 
     let error;
-    try {
-      switch (req.status.toUpperCase()) {
-        case "PAID":
-          error = await makePaidStatus(order);
-          break;
-        case "EXPIRED":
-          error = await makeFailedStatus(order);
-          break;
-        case "FAILED":
-          error = await makeFailedStatus(order);
-          break;
-        case "REFUND":
-          error = await makeRefundStatus(order);
-          break;
-        default:
-          throw new FailError("Unrecognized payment status", 400);
-      }
-      if (error) {
-        throw error.error;
-      }
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === "P2025") {
-          return NextResponse.json(
-            ...tripayResponse(false, `${e.meta.modelName} not found`, 404),
-          );
-        }
+    switch (req.status.toUpperCase()) {
+      case "PAID":
+        error = await makePaidStatus(order);
+        break;
+      case "EXPIRED":
+        error = await makeFailedStatus(order);
+        break;
+      case "FAILED":
+        error = await makeFailedStatus(order);
+        break;
+      case "REFUND":
+        error = await makeRefundStatus(order);
+        break;
+      default:
+        throw new FailError("Unrecognized payment status", 400);
+    }
+    if (error) {
+      throw error.error;
+    }
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      if (e.code === "P2025") {
         return NextResponse.json(
-          ...tripayResponse(false, prismaErrorCode[e.code], 409),
+          ...tripayResponse(false, `${e.meta.modelName} not found`, 404),
         );
       }
-
-      if (e instanceof FailError) {
-        return NextResponse.json(...tripayResponse(false, e.message, e.code));
-      }
-
-      return NextResponse.json(...tripayErrorResponse());
+      return NextResponse.json(
+        ...tripayResponse(false, prismaErrorCode[e.code], 409),
+      );
     }
 
-    return NextResponse.json({ success: true });
+    if (e instanceof FailError) {
+      return NextResponse.json(...tripayResponse(false, e.message, e.code));
+    }
+
+    return NextResponse.json(...tripayErrorResponse());
   }
+
+  return NextResponse.json({ success: true });
 }
 
 function tripayErrorResponse() {
